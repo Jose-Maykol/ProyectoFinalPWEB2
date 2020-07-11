9a57b04a937be1aa756fa544b24d45e800d615ea
Jose-Maykol <jpaniura@unsa.edu.pe>
2020-07-11 02:12:33 -0500

    Template y archivos estaticos index

--- ProyectoFinalPWEB2/settings.py
+++ ProyectoFinalPWEB2/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ProyectoFinalPWEB2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + "/Templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'Static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
