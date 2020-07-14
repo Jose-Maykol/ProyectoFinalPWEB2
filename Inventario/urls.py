8a892594d5aa1a453267b80132e5b8d1ba7e00b3
Maricarmen <ecoaquiracu@unsa.edu.pe>
2020-07-14 05:09:55 -0500

    implementado mi pagina web con diferentes vistas y arreglando los archivos html para que funcione

--- Inventario/urls.py
+++ Inventario/urls.py
@@ -5,5 +5,16 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('login',views.login, name = 'login'),
-    path('register', views.register, name = 'register')
+    path('register', views.register, name = 'register'),
+    #prueba para ir implementado la pagina
+    path('home', views.home, name = 'home'),
+    path('company', views.company, name='company'),
+    path('providers', views.providers, name='providers'),
+    path('payments', views.payments, name='payments'),
+    path('categories', views.categories, name='categories'),
+    #path('admins', views.admin, name='admin'),
+    path('client', views.client, name='client'),
+    path('products', views.products , name='products'),
+    path('sales', views.sales , name='sales'),
+    path('inventory', views.inventory , name='inventory'),
 ]
--- Inventario/views.py
+++ Inventario/views.py
@@ -40,3 +40,34 @@ def register(request):
         return redirect('/')
     else:
         return render(request,'register.html')
+
+#prueba para cargar la pagina
+def home(request):
+    return render(request,'home.html')
+
+def company(request):
+    return render(request,'company.html')
+
+def providers(request):
+    return render(request,'providers.html')
+
+def payments(request):
+    return render(request,'payments.html')
+
+def categories(request):
+    return render(request,'categories.html')
+
+def admin(request):
+    return render(request,'admin.html')
+
+def client(request):
+    return render(request,'client.html')
+
+def products(request):
+    return render(request,'products.html')
+
+def sales(request):
+    return render(request,'sales.html')
+
+def inventory(request):
+    return render(request,'inventory.html')
