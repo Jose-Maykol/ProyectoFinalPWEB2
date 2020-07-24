9b6ab6378fc6da3a59a27fafc314f43e6e94225c
Jose-Maykol <jpaniura@unsa.edu.pe>
2020-07-24 18:13:04 -0500

    Se  agrego la tabla proveedores

--- Inventario/admin.py
+++ Inventario/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Sell, Product , Operation
+from .models import Entry, Product , Sale, Provider
 
 # Register your models here.
 
-admin.site.register(Sell)
+admin.site.register(Entry)
 admin.site.register(Product)
-admin.site.register(Operation)
+admin.site.register(Sale)
+admin.site.register(Provider)
--- Inventario/models.py
+++ Inventario/models.py
@@ -6,27 +6,59 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     
     PRESENTATIONS = (('Caja','Caja'),('Bolsa','Bolsa'),('Frasco','Frasco'))
-    name = models.CharField(max_length= 200, null= True)
-    price_in  = models.FloatField(null= True)
-    price_out = models.FloatField(null= True)
-    cant = models.IntegerField(null= True)
-    presentation = models.CharField(max_length= 200, null= True, choices= PRESENTATIONS)
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
-    #category = models.CharField(max_length= 100, null= True)
+    name = models.CharField(max_length= 200, null= True, verbose_name= "Nombre del producto")
+    price_in  = models.FloatField(null= True, verbose_name= "Precio de entrada")
+    price_out = models.FloatField(null= True, verbose_name= "Precio de salida")
+    presentation = models.CharField(max_length= 200, null= True, choices= PRESENTATIONS, verbose_name= "Presentación")
+    user = models.ForeignKey(User, on_delete= models.CASCADE, verbose_name= "Usuario")
+    #Linea = models.ForeignKey(Linea,on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, null= True)
 
-class Operation(models.Model):
-    OPERATION = (('Entrada', 'Entrada'),('Salida','Salida'))
-    product_id = models.ForeignKey(Product, on_delete= models.CASCADE)
-    cant = models.IntegerField(null= True)
-    operation_type = models.CharField(max_length= 100, null= True, choices= OPERATION)
-    #sell_id =  
-    created_at = models.DateTimeField(auto_now_add=True, null= True)
+    def __str__(self):
+        return self.name
 
-class Sell(models.Model):
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural =  "Productos"
 
-    user_name =  models.ForeignKey(User,on_delete= models.CASCADE) 
-    operation_type = models.ForeignKey(Operation, on_delete= models.CASCADE) 
-    cant =  models.IntegerField(null= True)
-    cash = models.FloatField(null= True)
-    created_at = models.DateTimeField(auto_now_add=True, null= True)
+class Provider(models.Model):
+
+    name = models.CharField(max_length= 100, null= True, verbose_name= "Nombre de proveedor")
+    adrress = models.CharField(max_length= 100, null= True, verbose_name= "Direccion")
+    phone = models.CharField(max_length= 20, null= True, verbose_name= "Numero de telefono")
+    email =  models.EmailField(null= True, verbose_name= "E-mail")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Proveedor"
+        verbose_name_plural =  "Proveedores"
+
+class Entry(models.Model):
+
+    product = models.ForeignKey(Product, on_delete= models.CASCADE, verbose_name= "Producto", default= None)
+    provider = models.ForeignKey(Provider, on_delete= models.CASCADE, verbose_name= "Proveedor", default= None)
+    cant = models.IntegerField(null= True, verbose_name= "Cantidad")
+    created_at = models.DateTimeField(auto_now_add=True, null= True, verbose_name= "Fecha de movimiento")
+
+    class Meta:
+        verbose_name = "Entrada"
+        verbose_name_plural =  "Entradas"
+
+class Sale(models.Model):
+
+    user_name =  models.ForeignKey(User,on_delete= models.CASCADE, verbose_name= "Nombre de vendedor", default= None) 
+    product = models.ForeignKey(Product, on_delete= models.CASCADE, verbose_name= "Producto", default= None)
+    cant =  models.IntegerField(null= True, verbose_name= "Cantidad")
+    cash = models.FloatField(null= True, verbose_name= "Precio total")
+    created_at = models.DateTimeField(auto_now_add=True, null= True,verbose_name= "Fecha de venta")
+
+    class Meta:
+        verbose_name = "Salida"
+        verbose_name_plural =  "Salidas"
+
+
+
+
+
