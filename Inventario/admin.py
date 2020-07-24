401fa913f8a8ad2ead5f4356a434fd3378ffd9fe
Jose-Maykol <jpaniura@unsa.edu.pe>
2020-07-24 13:48:22 -0500

    Avanze bases de datos

--- Inventario/admin.py
+++ Inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Sell, Product , Operation
 
 # Register your models here.
+
+admin.site.register(Sell)
+admin.site.register(Product)
+admin.site.register(Operation)
--- Inventario/models.py
+++ Inventario/models.py
@@ -1,4 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Product(models.Model):
+    
+    PRESENTATIONS = (('Caja','Caja'),('Bolsa','Bolsa'),('Frasco','Frasco'))
+    name = models.CharField(max_length= 200, null= True)
+    price_in  = models.FloatField(null= True)
+    price_out = models.FloatField(null= True)
+    cant = models.IntegerField(null= True)
+    presentation = models.CharField(max_length= 200, null= True, choices= PRESENTATIONS)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    #category = models.CharField(max_length= 100, null= True)
+    created_at = models.DateTimeField(auto_now_add=True, null= True)
+
+class Operation(models.Model):
+    OPERATION = (('Entrada', 'Entrada'),('Salida','Salida'))
+    product_id = models.ForeignKey(Product, on_delete= models.CASCADE)
+    cant = models.IntegerField(null= True)
+    operation_type = models.CharField(max_length= 100, null= True, choices= OPERATION)
+    #sell_id =  
+    created_at = models.DateTimeField(auto_now_add=True, null= True)
+
+class Sell(models.Model):
+
+    user_name =  models.ForeignKey(User,on_delete= models.CASCADE) 
+    operation_type = models.ForeignKey(Operation, on_delete= models.CASCADE) 
+    cant =  models.IntegerField(null= True)
+    cash = models.FloatField(null= True)
+    created_at = models.DateTimeField(auto_now_add=True, null= True)
