1e43c46b516e409d7b1a3e0b3b17aca57882e179
Maricarmen <ecoaquiracu@unsa.edu.pe>
2020-07-26 20:43:28 -0500

    Creando mi tabla productos

--- Inventario/models.py
+++ Inventario/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Productos(models.Model):
+
+    id_producto = models.AutoField(primary_key=True)
+    nombre_producto = models.CharField(max_length=50)
+    descripcion_producto = models.TextField()
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2)
+    nivel_nuevaorden = models.IntegerField()
+    prod_descontinuado = models.BooleanField()
+    dias_espera = models.IntegerField()
+        
+
